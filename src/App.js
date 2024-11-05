58d487010b8596a19bfc7c818b9d823e99ca1c05
ThiyakuAbi20 <abiramythiyakesan280@gmail.com>
2024-11-05 22:43:09 +0530

    dashboard react finished

--- src/App.js
+++ src/App.js
@@ -1,44 +1,122 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import logo from './images/logo.png';
+import logo from './images/logo1.png';
 import homePage from './images/homePage.png';
+import brand1 from './images/brand1.png';
+import brand2 from './images/brand2.png';
+import brand3 from './images/brand3.png';
+import brand4 from './images/brand4.png';
+import social1 from './images/facebook.png.jpg';
+import social2 from './images/Youtube-logo.png.jpg';
+import social3 from './images/instagram.png.jpg';
+import social4 from './images/whatsapp.png.jpg';
+import address from './images/address.png';
+import email from './images/email.png';
+import phone from './images/phone.png';
+import driver from './images/driver-home.png';
 
 function App() {
+  const images = [brand1, brand2, brand3, brand4];
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [images.length]);
+
   return (
-    <div className="App">
-      <div className="navbar">
-      <img src={logo} alt="logo image" />
-        <h3>Delivery Portal</h3>
-        <ul>
+    <div className='header'>
+      <div className="App">
+        <div className="navbar">
+          <img src={logo} alt="logo image" />
+          <h3>Delivery Portal</h3>
+          <ul>
             <li><a href="#drivers">Drivers</a></li>
             <li><a href="#orders">Orders</a></li>
             <li><a href="#contact">Contact Us</a></li>
-      </ul>
-        <button type = "button">Let's Start</button>
+          </ul>
+          <button type="button">Let's Start</button>
+        </div>
       </div>
-      <div className="imagebar">
-      <img src={homePage} alt="grocery image" />
-      <button type = "button">Find Drivers</button>
-      <h4>Quickly identify available drivers to streamline your delivery operations and enhance efficiency.</h4>
-      </div>
+      <body>
+        <div className="imagebar">
+          <img src={homePage} alt="grocery image" />
+          <button type="button">Find Drivers</button>
+          <h4>Quickly identify available drivers to streamline your delivery operations and enhance efficiency.</h4>
+        </div>
 
-      <div className='textbar'>
-    <div className='text1bar'>
-        <h2>Drivers</h2>
-        <h1>Our Masterpieces</h1>
-        <p>At Northway, we pride ourselves on delivering exceptional service and quality. Our skilled drivers ensure timely deliveries, while our masterpieces showcase the finest craftsmanship and design. Together, they embody our commitment to excellence and customer satisfaction.</p>
-    </div>
-    <div className='text2bar'>
-        <h3>Mission Statement</h3>
-        <p>Our mission is to provide our community with fresh, high-quality groceries at competitive prices while prioritizing exceptional customer service. We are committed to supporting local farmers and sustainable practices, ensuring that our customers can find not only the essentials but also unique products that enhance their everyday lives. We strive to create a welcoming environment where customers feel valued and inspired to make healthy choices for themselves and their families.</p>
-    </div>
-    <div className='text3bar'>
-        <h3>Vision Statement</h3>
-        <p>Our vision is to be the leading grocery destination in our community, recognized for our commitment to quality, sustainability, and innovation. We envision a shopping experience that fosters community connections, promotes healthy living, and inspires our customers to explore new culinary adventures. By continually evolving and adapting to the needs of our customers, we aim to set the standard for excellence in the grocery industry.</p>
-    </div>
-</div>
-      
-    </div>
+        <div className='textbar'>
+          <div className='text1bar'>
+            <h2>Drivers</h2>
+            <h1>Our Masterpieces</h1>
+            <p>At Northway, we pride ourselves on delivering exceptional service and quality. Our skilled drivers ensure timely deliveries, while our masterpieces showcase the finest craftsmanship and design. Together, they embody our commitment to excellence and customer satisfaction.</p>
+          </div>
+          <div className='text2bar'>
+            <h3>Mission Statement</h3>
+            <p>Our mission is to provide our community with fresh, high-quality groceries at competitive prices while prioritizing exceptional customer service. We are committed to supporting local farmers and sustainable practices, ensuring that our customers can find not only the essentials but also unique products that enhance their everyday lives. We strive to create a welcoming environment where customers feel valued and inspired to make healthy choices for themselves and their families.</p>
+          </div>
+          <div className='text3bar'>
+            <h3>Vision Statement</h3>
+            <p>Our vision is to be the leading grocery destination in our community, recognized for our commitment to quality, sustainability, and innovation. We envision a shopping experience that fosters community connections, promotes healthy living, and inspires our customers to explore new culinary adventures. By continually evolving and adapting to the needs of our customers, we aim to set the standard for excellence in the grocery industry.</p>
+          </div>
+        </div>
+
+        <div className='form-and-slider'>
+          <div className='Formstyle'>
+            <h2>Add Your Driver Review . . .</h2>
+            <form>
+              <label htmlFor="review">Your Driver Review :</label>
+              <textarea id="review" name="review" rows="4" cols="50" placeholder="Write your review here..."></textarea>
+
+              <label htmlFor="name">Name :</label>
+              <input type="text" id="name" name="name" placeholder="Enter your name" required />
+
+              <label htmlFor="email">Email :</label>
+              <input type="email" id="email" name="email" placeholder="Enter your email" required />
+
+              <button type="submit">Add Review</button>
+            </form>
+          </div>
+
+          <div className='imageslider'>
+            <img src={images[currentIndex]} alt={`brand${currentIndex + 1}`} className="slider-image" />
+          </div>
+        </div>
+        </body>
+        <div className='footer'>
+          <div className='socialapp'>
+            <a href="https://web.facebook.com/northwayfamilymart/?_rdc=1&_rdr" target="_blank" rel="noopener noreferrer">
+              <img className='img1' src={social1} alt="Facebook" />
+            </a>
+            <a href="https://web.facebook.com/watch/?v=399479423869428" target="_blank" rel="noopener noreferrer">
+              <img className='img2' src={social2} alt="YouTube" />
+            </a>
+            <a href="https://www.instagram.com/explore/locations/338507396633298/northway-family-mart/" target="_blank" rel="noopener noreferrer">
+              <img className='img3' src={social3} alt="Instagram" />
+            </a>
+            <a href="https://wa.me/0777647645" target="_blank" rel="noopener noreferrer">
+              <img className='img4' src={social4} alt="WhatsApp" />
+            </a>
+          </div>
+          <button type="button">Contact us</button>
+          <div className='address'>
+            <img src={address} alt="logo image" />
+            <p>184, Parameswara Junction, Thirunelvely, Jaffna</p>
+          </div>
+          <div className='phone'>
+            <img src={phone} alt="logo image" />
+            <p>0212226002</p>
+          </div>
+          <div className='email'>
+            <img src={email} alt="logo image" />
+            <p>northwayfamilymart18@gmail.com</p>
+          </div>
+          
+          </div>
+          
+        </div>
   );
 }
 
